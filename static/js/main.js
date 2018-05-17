465dc718965257241a4c6ca00405472ea0e112b9
Schatzkin <schatzkin@eff.org>
2018-05-17 13:45:56 -0700

    tweaks to no js code, removed accordion from add-domain page for now (interfering with form)

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,12 @@
-window.onload = function(e){
+window.onload = function jsChange(e){
   var x = document.getElementsByClassName("js-yes");
   var i;
   for (i = 0; i < x.length; i++) {
-    alert(i);
       x[i].style.display = "inline-block";
   }
   var y = document.getElementsByClassName("js-no");
   var i;
   for (i = 0; i < y.length; i++) {
-    alert(i);
       y[i].style.display = "none";
   }
 }
