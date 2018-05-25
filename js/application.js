06d23af2728546422bd43af440bcb3d8f5ce40aa
Schatzkin <schatzkin@eff.org>
2018-05-25 14:09:25 -0700

    added js code to style 'do you manage this email server?' dropdown

--- js/application.js
+++ js/application.js
@@ -3,17 +3,87 @@ $(function() {
   $('.js-yes').show();
 
   $('.accordion-checkbox').click(function(){
-      if (this.checked) {
-          $('.accordion-title h4').css('color', '#666');
-          $('.accordion-title h4 a').css('color', '#666');
-      }
-      else {
-          $('.accordion-title h4').css('color', '#000');
-          $('.accordion-title h4 a').css('color', '#000');
-      }
+    if (this.checked) {
+      $('.accordion-title h4').css('color', '#666');
+      $('.accordion-title h4 a').css('color', '#666');
+    }
+    else {
+      $('.accordion-title h4').css('color', '#000');
+      $('.accordion-title h4 a').css('color', '#000');
+    }
   })
 
   $(".add-another").click(function(){
     $(".add-domains").prepend('<input type="text" value="mx.domain.org"></input>');
   })
+
+  //select menu
+  /*
+  Reference: http://jsfiddle.net/BB3JK/47/
+  */
+
+  // Iterate over each select element
+  $('#manage').each(function () {
+
+    // Cache the number of options
+    var $this = $(this),
+    numberOfOptions = $(this).children('option').length;
+
+    // Hides the select element
+    $this.addClass('s-hidden');
+
+    // Wrap the select element in a div
+    $this.wrap('<div class="select"></div>');
+
+    // Insert a styled div to sit over the top of the hidden select element
+    $this.after('<div class="styledSelect"></div>');
+
+    // Cache the styled div
+    var $styledSelect = $this.next('div.styledSelect');
+
+    // Show the first select option in the styled div
+    $styledSelect.text($this.children('option').eq(0).text());
+
+    // Insert an unordered list after the styled div and also cache the list
+    var $list = $('<ul />', {
+      'class': 'options'
+    }).insertAfter($styledSelect);
+
+    // Insert a list item into the unordered list for each select option
+    for (var i = 0; i < numberOfOptions; i++) {
+      $('<li />', {
+        text: $this.children('option').eq(i).text(),
+        rel: $this.children('option').eq(i).val()
+      }).appendTo($list);
+    }
+
+    // Cache the list items
+    var $listItems = $list.children('li');
+
+    // Show the unordered list when the styled div is clicked (also hides it if the div is clicked again)
+    $styledSelect.click(function (e) {
+      e.stopPropagation();
+      $('div.styledSelect.active').each(function () {
+        $(this).removeClass('active').next('ul.options').hide();
+      });
+      $(this).toggleClass('active').next('ul.options').toggle();
+    });
+
+    // Hides the unordered list when a list item is clicked and updates the styled div to show the selected list item
+    // Updates the select element to have the value of the equivalent option
+    $listItems.click(function (e) {
+      e.stopPropagation();
+      $styledSelect.text($(this).text()).removeClass('active');
+      $this.val($(this).attr('rel'));
+      $list.hide();
+      /* alert($this.val()); Uncomment this for demonstration! */
+    });
+
+    // Hides the unordered list when clicking outside of it
+    $(document).click(function () {
+      $styledSelect.removeClass('active');
+      $list.hide();
+    });
+
+  });
 });
