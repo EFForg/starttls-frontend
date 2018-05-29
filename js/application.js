7194c928935b53b24f47332047b442e13d0a206e
Schatzkin <schatzkin@eff.org>
2018-05-29 16:07:25 -0700

    adding copy to clipboard basic code

--- js/application.js
+++ js/application.js
@@ -17,3 +17,35 @@ $(function() {
     $(".add-domains").prepend('<input type="text" value="mx.domain.org"></input>');
   })
 });
+
+/*
+	Copy text from any appropriate field to the clipboard
+  By Craig Buckler, @craigbuckler
+  use it, abuse it, do whatever you like with it!
+*/
+$(function() {
+	'use strict';
+  document.body.addEventListener('click', copy, true);
+	function copy(e) {
+    // find target element
+    var
+      t = e.target,
+      c = t.dataset.copytarget,
+      inp = (c ? document.querySelector(c) : null);
+
+    // is element selectable?
+    if (inp && inp.select) {
+      inp.select();
+      try {
+        document.execCommand('copy');
+        inp.blur();
+        // copied animation
+        t.classList.add('copied');
+        setTimeout(function() { t.classList.remove('copied'); }, 1500);
+      }
+      catch (err) {
+        alert('please press Ctrl/Cmd+C to copy');
+      }
+    }
+	}
+});
