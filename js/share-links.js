8c12555df5783b3d2ba4a678b0468f3265a60c3b
Schatzkin <schatzkin@eff.org>
2018-05-31 14:48:19 -0700

    adding share links for results

---
+++ js/share-links.js
@@ -0,0 +1,20 @@
+$(function() {
+  var url = window.location;
+  $('.share-links a.share-twitter').click(function (e) {
+    e.preventDefault();
+    tweetShare = "https://twitter.com/share?url="+encodeURIComponent(url);
+    window.open(tweetShare,'', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=500,width=650');
+  });
+
+  $('.share-links a.share-facebook').click(function (e) {
+    e.preventDefault();
+    facebookShare = "https://www.facebook.com/sharer/sharer.php?u="+encodeURIComponent(url);
+    window.open(facebookShare, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=500,width=650');
+  });
+
+  $('.share-links a.share-google').click(function (e) {
+    e.preventDefault();
+    googleShare = "https://plus.google.com/share?url="+encodeURIComponent(url);
+    window.open(googleShare, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=436,width=400');
+  });
+});
