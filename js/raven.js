9c1b206f79b87600be2624b3d487f2993a8a21fd
vivian <vivian@eff.org>
2018-06-06 15:26:58 -0700

    fixup! Load environment for hugo

--- js/raven.js
+++ js/raven.js
@@ -1,6 +1,6 @@
-console.log("loaded")
 var dsn = document.querySelector('script[data-sentry-dsn]')
   .getAttribute('data-sentry-dsn');
 var id = document.querySelector('script[data-sentry-id]')
   .getAttribute('data-sentry-id');
-Raven.config('https://' + dsn + '@sentry.eff.org/' + id).install()
+if (dsn.length > 0 && id.length > 0)
+  Raven.config('https://' + dsn + '@sentry.eff.org/' + id).install();
