e03714633f0a0ae484a53e2fa25eeb39447a5389
vivian <vivian@eff.org>
2018-06-05 17:13:34 -0700

    Remove dead code

--- js/application.js
+++ js/application.js
@@ -22,12 +22,6 @@ $(function() {
     $(linkhref).parent().next().css("display","block");
   });
 
-
-  // add another domain name to the add domain page
-  $(".add-another").click(function(){
-    $(".add-domains").prepend('<input type="text" value="mx.domain.org"></input>');
-  });
-
   // share link social media icon in footer
   $(".copy-url").click(function(){
     urlCopied.innerHTML = window.location.href;
