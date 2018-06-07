bc002662cc48b9a3209d23871ebda8c73c9e1098
Schatzkin <schatzkin@eff.org>
2018-06-07 15:17:19 -0700

    fixes

--- js/accordion.js
+++ js/accordion.js
@@ -16,15 +16,14 @@ $(function() {
   var faqUrl = window.location.href;
   var faqPage = "faq/";
   if(faqUrl.indexOf(faqPage) != -1){
-    faqAnchor = "#";
-      if(faqUrl.indexOf(faqAnchor) != -1){
-        //get anchor link, open panel
-        var n = faqUrl.lastIndexOf('#');
-        var openPanel = faqUrl.substring(n);
-        $(openPanel).parent().next().css({
-                   "max-height" : "3000px",
-                   "margin-top" : "0" });
+    var faqAnchor = "#";
+    if(faqUrl.indexOf(faqAnchor) != -1){
+      //get anchor link, open panel
+      var n = faqUrl.lastIndexOf('#');
+      var openPanel = faqUrl.substring(n);
+      $(openPanel).parent().next().css({
+        "max-height" : "3000px",
+        "margin-top" : "0" });
       }
-  }
-
+    }
 });
