a14913b8d7ee5ba2f33a0873ccc56103ed45e229
Vivian Brown <vivian@eff.org>
2019-02-08 15:10:35 -0800

    MX hostnames no longer required

--- js/queue-form.js
+++ js/queue-form.js
@@ -46,17 +46,21 @@ function init_form() {
   init_toggle_mta_sts();
   init_add_another_mx();
   var mta_sts = url.searchParams.get('mta_sts');
-  if (mta_sts == 'true')
-    $('#mta-sts-input').prop('checked', true)
+  if (mta_sts == 'true') {
+    $('#mta-sts-input').prop('checked', true);
+    $('#mx-hostnames').hide();
+  }
 }
 
 function init_toggle_mta_sts() {
   $mta_sts = $('#mta-sts-input');
   $mta_sts.change(function() {
     if ($mta_sts.prop('checked'))
-      $('#mx-hostnames').slideUp();
+      $('#mx-hostnames').slideUp()
+        .find('input').attr('disabled', true);
     else
-      $('#mx-hostnames').slideDown();
+      $('#mx-hostnames').slideDown()
+        .find('input').attr('disabled', false);
   });
 }
 
