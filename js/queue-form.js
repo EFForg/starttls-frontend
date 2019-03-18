c580af6a9425ba5aed8880517d32d6c796e8c362
Vivian Brown <vivian@eff.org>
2019-03-18 15:08:05 -0700

    Submit MTA-STS boolean from Add Domain form (#259)

--- js/queue-form.js
+++ js/queue-form.js
@@ -3,11 +3,7 @@
  */
 $(function() {
   var $form = $("#queue");
-  init_add_another_mx_hostname();
-
-  var url = new URL(window.location.href);
-  var domain = url.searchParams.get('domain');
-  if (domain) $('#domain-input').val(domain)
+  init_form();
 
   $form.submit(function(e) {
     $form.find(".errors").hide();
@@ -41,18 +37,47 @@ $(function() {
   });
 });
 
-function init_add_another_mx_hostname() {
-  var $add_another = $("#queue .add-another"),
-    $mx_domains = $("#queue .mx-domain");
+function init_form() {
+  var url = new URL(window.location.href);
+  var domain = url.searchParams.get('domain');
+  if (domain) {
+    $('#domain-input').val(domain);
+    $('.your-domain').text(domain);
+  }
+
+  init_toggle_mta_sts();
+  init_add_another_mx();
+  var mta_sts = url.searchParams.get('mta_sts');
+  if (mta_sts == 'true') {
+    $('#mta-sts-input').prop('checked', true);
+    $('#mx-hostnames').hide();
+  }
+}
+
+function init_toggle_mta_sts() {
+  $mta_sts = $('#mta-sts-input');
+  $mta_sts.change(function() {
+    if ($mta_sts.prop('checked'))
+      $('#mx-hostnames').slideUp()
+        .find('input').attr('disabled', true);
+    else
+      $('#mx-hostnames').slideDown()
+        .find('input').attr('disabled', false);
+  });
+}
+
+function init_add_another_mx() {
+  var $add_another = $('#queue .add-another'),
+    $mxs = $('#queue .mx-domain');
 
   // Hide all but the first mx-domain field
-  $mx_domains.slice(1).hide();
+  $mxs.slice(1).hide();
 
   $add_another.click(function() {
-    var $hidden_fields = $mx_domains.filter(":hidden");
+    var $hidden_fields = $mxs.filter(':hidden');
     $hidden_fields.first().show();
     if ($hidden_fields.length <= 1)
       // Showed the last field, no more to show
-      $add_another.hide()
+      $add_another.attr('disabled', true);
   });
 }
