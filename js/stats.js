c3a7fd37b075bd5c56509f4828789e3c42f64546
Vivian Brown <vivian@eff.org>
2019-05-23 18:11:19 -0700

    Style tooltips

--- js/stats.js
+++ js/stats.js
@@ -9,12 +9,16 @@ $(function() {
     url: hostname + '/api/stats',
   })
   .done(function(resp) {
-    load_chart('top-million-chart', resp.response.top_million, '#f938bd');
-    load_chart('visitors-chart', resp.response.local, '#20c9c9');
+    load_chart('top-million-chart', resp.response.top_million, '#f938bd', function(item) {
+      return item.value + ' domains';
+    });
+    load_chart('visitors-chart', resp.response.local, '#20c9c9', function(item) {
+      return item.value + '% of scans';
+    });
   });
 });
 
-function load_chart(id, data, color) {
+function load_chart(id, data, color, label_cb) {
   var ctx = document.getElementById(id).getContext('2d');
   var chart = new Chart(ctx, {
     type: 'line',
@@ -34,8 +38,21 @@ function load_chart(id, data, color) {
       },
       scales: {
         xAxes: [{
-          type: 'time'
+          type: 'time',
+          time: {
+            unit: 'day',
+            tooltipFormat: 'MMM D'
+          },
         }],
+      },
+      tooltips: {
+        custom: function(tooltip) {
+          if (!tooltip) return;
+          tooltip.displayColors = false;
+        },
+        callbacks: {
+          label: label_cb
+        }
       }
     }
   });
